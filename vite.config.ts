b35c210c425ee91817faa7a8991b5a423fa94486
BL001 <>
2024-04-02 11:38:39 +0800

    開發 更換 title 以及 ico

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,19 @@ import react from '@vitejs/plugin-react'
 import path from 'path';
 
 // https://vitejs.dev/config/
+const htmlPlugin = () => {
+  return {
+    name: 'html-transform',
+    transformIndexHtml(html) {
+      return html.replace(
+        /<title>(.*?)<\/title>/,
+        `<title>TheF2e 立委競選官網 - 陳英渝</title>`,
+      )
+    },
+  }
+}
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), htmlPlugin()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
